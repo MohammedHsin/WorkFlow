dc97f3311998e34433c47b4277a82258bf34dcfa
MohammedHsin <mohammedhsin.technological@gmail.com>
2023-04-12 18:05:03 +0300

    set up the custom circular progress bar

--- app/src/main/java/com/example/workflow/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/example/workflow/presentation/ui/theme/Color.kt
@@ -6,3 +6,7 @@ val Purple200 = Color(0xFF0080E6)
 val Purple500 = Color(0xFF006AFF)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val blue = Color(0xFF140ddb)
+val brightBlue = Color(0xFF027cf5)
+val darkBlue = Color(0xFF18192b)
